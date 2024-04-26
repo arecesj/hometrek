f3b9ccc54d30d34e6f8cdf89a23443ad3f4fbc29
arecesj <jdareces@gmail.com>
2024-04-26 13:39:55 -0400

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import { Mulish } from "next/font/google";
 import "./globals.css";
@@ -22,6 +23,7 @@ export default function RootLayout({
         <AppWrapper>
           {children}
           <Analytics />
+          <SpeedInsights />
         </AppWrapper>
       </body>
     </html>
