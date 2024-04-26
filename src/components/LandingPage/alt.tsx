16e198d153c2ac0a9c2080c047f53cea29cd61c4
arecesj <jdareces@gmail.com>
2024-04-26 08:43:52 -0400

    move window img

--- src/components/LandingPage/alt.tsx
+++ src/components/LandingPage/alt.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-import window from "../../app/images/window.jpg"
+import window from "../../images/window.jpg"
 
 const Alt = () => {
   return (
