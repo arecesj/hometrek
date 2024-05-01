909093e3e65d511b3c2365627379eaaef8c885b3
arecesj <jdareces@gmail.com>
2024-05-01 17:25:38 -0400

    disable

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,5 @@
+/* eslint-disable padded-blocks */
+
 import { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
