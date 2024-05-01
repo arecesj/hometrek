09c051b0e9391abdde1dc6c40e8e948fe3144fa6
arecesj <jdareces@gmail.com>
2024-05-01 10:01:54 -0400

    add Agg experience to landing page

--- src/components/LandingPage/Navigation.tsx
+++ src/components/LandingPage/Navigation.tsx
@@ -22,9 +22,12 @@ const Navigation = () => {
           </ul>
         </div> */}
         
-        <div className="hidden lg:flex lg:items-center text-[hsl(0,0%,41%)]"> 
+        <div className="hidden lg:flex lg:items-center text-[hsl(0,0%,41%)] space-x-2"> 
           <Button asChild className="lg:text-[hsl(0,0%,41%)] lg:bg-transparent border-2 border-[hsl(0,0%,41%)] hover:text-white hover:border-[hsl(0,0%,8%)] py-3 px-6 rounded-xl">
-            <Link href="/agg/lenders" className="text-base font-medium">Get Started</Link>
+            <Link href="/agg/lenders" className="text-base font-medium">Track</Link>
+          </Button>
+          <Button asChild className="lg:text-[hsl(0,0%,41%)] lg:bg-transparent border-2 border-[hsl(0,0%,41%)] hover:text-white hover:border-[hsl(0,0%,8%)] py-3 px-6 rounded-xl">
+            <Link href="/trek/lenders" className="text-base font-medium">Find lenders</Link>
           </Button>
         </div>
         {/* desktop menu end */}
