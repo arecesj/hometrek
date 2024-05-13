ced53c6f2b7f641a7f205ae5de7417a4a8cbfd4c
arecesj <jdareces@gmail.com>
2024-05-13 12:33:58 -0400

    fix dashboard bug

--- src/components/Manage/Dashboard/index.tsx
+++ src/components/Manage/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState } from "react"
 import { useSession } from "next-auth/react"
 import { useAppContext } from "@/context"
 import SubHeader from "../Subheader"
@@ -9,13 +10,28 @@ import CostBreakdown from "./CostBreakdown.tsx"
 import TaskTable from "../TaskTable"
 import { generateNewUserTasks } from "@/constants/newUserTasks"
 import { isUserAuthenticated } from "@/lib/utils"
+import LoadingSpinner from "@/components/LoadingSpinner"
 
 const Dashboard = () => {
   const { data: session, status } = useSession()
-  const { homeClosingContext, setRouteContext } = useAppContext()
-  const tasks = generateNewUserTasks(homeClosingContext);
+  const { homeClosingContext, setHomeClosingContext, setRouteContext } = useAppContext()
+  const [isLoading, setLoading] = useState<boolean>(true)
+  
+  useEffect(() => {
+    setRouteContext(manageRouteName.DASHBOARD)
+    
+    if(!homeClosingContext.tasks?.length) {
+      const tasks = generateNewUserTasks(homeClosingContext);
+      
+      setHomeClosingContext({
+        ...homeClosingContext,
+        tasks
+      })
+    }
 
-  useEffect(() => setRouteContext(manageRouteName.DASHBOARD), [])
+    setLoading(false)
+  }, [])
+  
   return (
     <div>
       <SubHeader
@@ -25,12 +41,16 @@ const Dashboard = () => {
         previousButtonHref={""}
         showCreateProfileButton={!isUserAuthenticated(status)}
       />
-      <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
         <div className="grid auto-rows-max items-start md:gap-8 lg:col-span-2">
-          <TaskTable tasks={tasks} />
+          <TaskTable tasks={homeClosingContext.tasks} />
         </div>
         <CostBreakdown />
       </div>
+      )}
       
     </div>
   )
