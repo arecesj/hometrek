ea0669c8f24728d80c63138bb64b1d29c4217980
arecesj <jdareces@gmail.com>
2024-05-14 20:38:42 -0400

    mapMortgageDetails util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -67,3 +67,33 @@ export const subtractNumStrings = (ns1: string, ns2: string) => {
 
 
 export const isUserAuthenticated = (status: string) => status === "authenticated"
+
+export const mapMortgageDetails = (mortgage) => {
+  return {
+    accountId: mortgage.account_id,
+    accountNumber: mortgage.account_number,
+    currentLateFee: mortgage.current_late_fee,
+    escrowBalance: mortgage.escrow_balance,
+    hasPMI: mortgage.has_pmi,
+    hasPrepaymentPenalty: mortgage.has_prepayment_penalty,
+    interestRatePercentage: mortgage.interest_rate.percentage,
+    interestRateType: mortgage.interest_rate.type,
+    lastPaymentAmount: mortgage.last_payment_amount,
+    lastPaymentDate: mortgage.last_payment_date,
+    loanTerm: mortgage.loan_term,
+    loanTypeDescription: mortgage.loan_type_description,
+    maturityDate: mortgage.maturity_date,
+    nextMonthlyPayment: mortgage.next_monthly_payment,
+    nextPaymentDueDate: mortgage.next_payment_due_date,
+    originationDate: mortgage.origination_date,
+    originationPrincipalAmount: mortgage.origination_principal_amount,
+    pastDueAmount: mortgage.past_due_amount,
+    city: mortgage.property_address.city,
+    country: mortgage.property_address.country,
+    postalCode: mortgage.property_address.post_code,
+    region: mortgage.property_address.region,
+    street: mortgage.property_address.street,
+    ytdInterestPaid: mortgage.ytd_interest_paid,
+    ytdPrincipalPaid: mortgage.ytd_principal_paid,
+  }
+}
